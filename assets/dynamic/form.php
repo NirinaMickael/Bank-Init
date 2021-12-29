b708b7873dda85c1cd10055781b0e6e6edbd0c40
NirinaMickarl <todisoanirinamickael@gmail.com>
2021-12-29 14:44:48 +0300

    modification

--- assets/dynamic/form.php
+++ assets/dynamic/form.php
@@ -5,13 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style/style.css">
-    <link rel="stylesheet" href="../style/bootstrap-5.0.2-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../bootstrap-5.0.2-dist/css/bootstrap.min.css">    
     <title>formulaire</title>
 </head>
 <body>
    <main class="container-fluid m-auto">
         <form class="needs-validations border">
-            <label class="form-label" for="responsable">Responsable:</label>
+            <label class="form-label" for="responsable">Responsable</label>
             <select class="form-select form-check"  id="responsable">
                 <option value="1">Agent1</option>
                 <option value="2">Agent2</option>
