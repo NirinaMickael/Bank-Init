ec0670eb7a63bc1eff2238d7859a24b6b0f2da3c
NirinaMickarl <todisoanirinamickael@gmail.com>
2022-01-05 06:16:01 +0300

    Nouvelle version

--- assets/dynamic/header.php
+++ assets/dynamic/header.php
@@ -1,4 +1,3 @@
-<?php  require_once('functionlist.php')?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- assets/dynamic/table.php
+++ assets/dynamic/table.php
@@ -1,97 +1,51 @@
-<table class="table table-striped table-bordered table-responsive table-sm" cellspacing="0">
+<table id="table" class="table table-striped table-bordered table-responsive table-sm" cellspacing="0">
         <thead>
         <tr>
-            <th >
+            <th>
                 <a>Name</a>
                 <span class="float-right">
-                    <span id="arrowUp">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="16" fill="currentColor" class="bi bi-arrow-down  mt-1" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
-                    </svg>
-                    </span>
-                    </span>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="16" fill="currentColor" class="bi bi-arrow-up  mt-1" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
-                    </svg>
+                    <img src="assets/images/arrow-down.svg" id ="arrowdown" alt="arrow"  srcset="">
+                    <img src="assets/images/arrow-up.svg" id ="arrowup" alt="arrow"  srcset="">
                 </span>
             </th>
             <th>
                 <a>Position</a>
                 <span class="float-right">
-                     <span id="arrowUp">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="16" fill="currentColor" class="bi bi-arrow-down  mt-1" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
-                    </svg>
-                    </span>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="16" fill="currentColor" class="bi bi-arrow-up  mt-1" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
-                    </svg>
+                    <img src="assets/images/arrow-down.svg" id ="arrowdown" alt="arrow"  srcset="">
+                    <img src="assets/images/arrow-up.svg" id ="arrowup" alt="arrow"  srcset="">
                 </span>
             </th>            
             <th>
                 <a>Office</a>
                 <span class="float-right">
-                     <span id="arrowUp">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="16" fill="currentColor" class="bi bi-arrow-down  mt-1" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
-                    </svg>
-                    </span>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="16" fill="currentColor" class="bi bi-arrow-up  mt-1" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
-                    </svg>
+                    <img src="assets/images/arrow-down.svg" id ="arrowdown" alt="arrow"  srcset="">
+                    <img src="assets/images/arrow-up.svg" id ="arrowup" alt="arrow"  srcset="">
                 </span>
             </th>            
             <th>
                 <a>Age</a>
                 <span class="float-right">
-                     <span id="arrowUp">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="16" fill="currentColor" class="bi bi-arrow-down  mt-1" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
-                    </svg>
-                    </span>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="16" fill="currentColor" class="bi bi-arrow-up  mt-1" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
-                     </svg>
+                    <img src="assets/images/arrow-down.svg" id ="arrowdown" alt="arrow"  srcset="">
+                    <img src="assets/images/arrow-up.svg" id ="arrowup" alt="arrow"  srcset="">
                 </span>
             </th>           
             <th>
                 <a>Start Date</a>
                 <span class="float-right">
-                     <span id="arrowUp">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="16" fill="currentColor" class="bi bi-arrow-down  mt-1" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
-                    </svg>
-                    </span>
-                     <svg xmlns="http://www.w3.org/2000/svg" width="10" height="16" fill="currentColor" class="bi bi-arrow-up  mt-1" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
-                    </svg>
+                    <img src="assets/images/arrow-down.svg" id ="arrowdown" alt="arrow"  srcset="">
+                    <img src="assets/images/arrow-up.svg" id ="arrowdown" alt="arrow"  srcset="">
                 </span>
             </th>
             <th>
                 <a>Salary</a>
                 <span class="float-right">
-                     <span id="arrowUp">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="16" fill="currentColor" class="bi bi-arrow-down  mt-1" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
-                    </svg>
-                    </span>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="16" fill="currentColor" class="bi bi-arrow-up  mt-1" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
-                    </svg>
+                    <img src="assets/images/arrow-down.svg" id ="arrowdown" alt="arrow"  srcset="">
+                    <img src="assets/images/arrow-up.svg" id ="arrowdown" alt="arrow"  srcset="">
                 </span> 
             </th>
         </tr>
         </thead>
-        <tbody>
-        <?php
-            foreach($data as $item){
-                echo "<tr>";
-                    foreach($item as $value){
-                        echo printItemTable($value);
-                    }
-                echo "</tr>";
-            }
-        ?>    
+        <tbody>    
         </tbody>
             
         <tfoot>
